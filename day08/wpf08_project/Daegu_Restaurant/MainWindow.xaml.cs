2b5bd7182e527163b71347dafaa8a3a38d6a976f
JinyongSon <wlsdydths@naver.com>
2024-05-17 17:27:39 +0900

    개인 프로젝트 거의 완성

--- day08/wpf08_project/Daegu_Restaurant/MainWindow.xaml.cs
+++ day08/wpf08_project/Daegu_Restaurant/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -25,6 +26,8 @@ namespace Daegu_Restaurant
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private bool isFavorite = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -227,6 +230,216 @@ namespace Daegu_Restaurant
             {
                 await this.ShowMessageAsync("검색 오류", $"검색 중 오류가 발생했습니다: {ex.Message}");
             }
+
+            BtnViewFavorite_Click(sender, e); // 저장후 저장된 즐겨찾기 바로보기
+
+        }
+
+        private async void BtnAddFavorite_Click(object sender, RoutedEventArgs e)
+        {
+            if (GrdResult.SelectedItems.Count == 0)
+            {
+                await this.ShowMessageAsync("즐겨찾기", "추가할 맛집을 선택하세요(복수선택가능).");
+                return;
+            }
+
+            if (isFavorite == true) // 즐겨찾기 보기한 뒤 영화를 다시 즐겨찾기하려고할때 막음.
+            {
+                await this.ShowMessageAsync("즐겨찾기", "이미 즐겨찾기한 맛집입니다.");
+                return;
+            }
+
+            var addMovieItems = new List<Favorite>();
+            foreach (Restaurant item in GrdResult.SelectedItems)
+            {
+                addMovieItems.Add(new Favorite()
+                {
+                    Id = item.Id,
+                    Cnt = item.Cnt,
+                    Opendata_id = item.Opendata_id,
+                    Gng_cs = item.Gng_cs,
+                    Fd_cs = item.Fd_cs,
+                    Bz_nm = item.Bz_nm,
+                    Tlno = item.Tlno,
+                    Mbz_hr = item.Mbz_hr,
+                    Seat_cnt = item.Seat_cnt,
+                    Pkpl = item.Pkpl,
+                    Hp = item.Hp,
+                    Psb_frn = item.Psb_frn,
+                    Bkn_yn = item.Bkn_yn,
+                    Infn_fcl = item.Infn_fcl,
+                    Brft_yn = item.Brft_yn,
+                    Dssrt_yn = item.Dssrt_yn,
+                    Mnu = item.Mnu,
+                    Smpl_desc = item.Smpl_desc,
+                    Sbw = item.Sbw,
+                    Bus = item.Bus,
+                });
+            }
+
+            Debug.WriteLine(addMovieItems.Count);
+            try
+            {
+                var insRes = 0;
+
+                using (SqlConnection conn = new SqlConnection(Helpers.Common.CONNSTRING))
+                {
+                    conn.Open();
+
+                    foreach (Favorite item in addMovieItems)
+                    {
+                        // 저장되기 전에 이미 저장된 데이터인지 확인 후 
+                        SqlCommand chkCmd = new SqlCommand(Favorite.CHECK_QUERY, conn);
+                        chkCmd.Parameters.AddWithValue("@Id", item.Id);
+                        var cnt = Convert.ToInt32(chkCmd.ExecuteScalar()); // COUNT(*) 등의 1row, 1coloumn값을 리턴할때
+
+                        if (cnt == 1) continue;  // 이미 데이터가 있으면 패스
+
+                        SqlCommand cmd = new SqlCommand(Models.Favorite.INSERT_QUERY, conn);
+                        cmd.Parameters.AddWithValue("@Cnt", item.Cnt);
+                        cmd.Parameters.AddWithValue("@Opendata_id", item.Opendata_id);
+                        cmd.Parameters.AddWithValue("@Gng_cs", item.Gng_cs);
+                        cmd.Parameters.AddWithValue("@Fd_cs", item.Fd_cs);
+                        cmd.Parameters.AddWithValue("@Bz_nm", item.Bz_nm);
+                        cmd.Parameters.AddWithValue("@Tlno", item.Tlno);
+                        cmd.Parameters.AddWithValue("@Mbz_hr", item.Mbz_hr);
+                        cmd.Parameters.AddWithValue("@Seat_cnt", item.Seat_cnt);
+                        cmd.Parameters.AddWithValue("@Pkpl", item.Pkpl);
+                        cmd.Parameters.AddWithValue("@Hp", item.Hp);
+                        cmd.Parameters.AddWithValue("@Psb_frn", item.Psb_frn);
+                        cmd.Parameters.AddWithValue("@Bkn_yn", item.Bkn_yn);
+                        cmd.Parameters.AddWithValue("@Infn_fcl", item.Infn_fcl);
+                        cmd.Parameters.AddWithValue("@Brft_yn", item.Brft_yn);
+                        cmd.Parameters.AddWithValue("@Dssrt_yn", item.Dssrt_yn);
+                        cmd.Parameters.AddWithValue("@Mnu", item.Mnu);
+                        cmd.Parameters.AddWithValue("@Smpl_desc", item.Smpl_desc);
+                        cmd.Parameters.AddWithValue("@Sbw", item.Sbw);
+                        cmd.Parameters.AddWithValue("@Bus", item.Bus);
+
+                        insRes += cmd.ExecuteNonQuery(); // 데이터 하나마다 INSERT쿼리 실행
+                    }
+                }
+
+                if (insRes == addMovieItems.Count)
+                {
+                    await this.ShowMessageAsync("즐겨찾기", $"즐겨찾기 {insRes}건 저장성공!");
+                }
+                else
+                {
+                    await this.ShowMessageAsync("즐겨찾기", $"즐겨찾기 {addMovieItems.Count}건중 {insRes}건 저장성공!");
+                }
+            }
+            catch (Exception ex)
+            {
+                await this.ShowMessageAsync("오류", $"즐겨찾기 오류 {ex.Message}");
+            }
+        }
+
+        private async void BtnViewFavorite_Click(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = null;  // 데이터그리드에 보낸 데이터를 모두 삭제
+            TxtMenuName.Text = string.Empty;
+
+            List<Favorite> favorites = new List<Favorite>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Helpers.Common.CONNSTRING))
+                {
+                    conn.Open();
+
+                    var cmd = new SqlCommand(Models.Favorite.SELECT_QUERY, conn);
+                    var adapter = new SqlDataAdapter(cmd);
+                    var dSet = new DataSet();
+                    adapter.Fill(dSet, "Favorite");
+
+                    foreach (DataRow row in dSet.Tables["Favorite"].Rows)
+                    {
+                        var favorite = new Favorite()
+                        {
+                            Id = Convert.ToInt32(row["Id"]),
+                            Cnt = Convert.ToInt32(row["Cnt"]),
+                            Opendata_id = Convert.ToString(row["Opendata_id"]),
+                            Gng_cs = Convert.ToString(row["Gng_cs"]),
+                            Fd_cs = Convert.ToString(row["Fd_cs"]),
+                            Bz_nm = Convert.ToString(row["Bz_nm"]),
+                            Tlno = Convert.ToString(row["Tlno"]),
+                            Mbz_hr = Convert.ToString(row["Mbz_hr"]),
+                            Seat_cnt = Convert.ToString(row["Seat_cnt"]),
+                            Pkpl = Convert.ToString(row["Pkpl"]),
+                            Hp = Convert.ToString(row["Hp"]),
+                            Psb_frn = Convert.ToString(row["Psb_frn"]),
+                            Bkn_yn = Convert.ToString(row["Bkn_yn"]),
+                            Infn_fcl = Convert.ToString(row["Infn_fcl"]),
+                            Brft_yn = Convert.ToString(row["Brft_yn"]),
+                            Dssrt_yn = Convert.ToString(row["Dssrt_yn"]),
+                            Mnu = Convert.ToString(row["Mnu"]),
+                            Smpl_desc = Convert.ToString(row["Smpl_desc"]),
+                            Sbw = Convert.ToString(row["Sbw"]),
+                            Bus = Convert.ToString(row["Bus"]),
+                        };
+
+                        favorites.Add(favorite);
+                    }
+
+                    this.DataContext = favorites;
+                    isFavorite = true; // 즐겨찾기 DB에서 
+                    StsResult.Content = $"즐겨찾기 {favorites.Count}건 조회완료";
+                }
+            }
+            catch (Exception ex)
+            {
+                await this.ShowMessageAsync("오류", $"즐겨찾기 조회오류 {ex.Message}");
+            }
+        }
+
+        private async void BtnDelFavorite_Click(object sender, RoutedEventArgs e)
+        {
+            if (isFavorite == false)
+            {
+                await this.ShowMessageAsync("삭제", "즐겨찾기한 맛집이 아닙니다.");
+                return;
+            }
+
+            if (GrdResult.SelectedItems.Count == 0)
+            {
+                await this.ShowMessageAsync("삭제", "삭제할 맛집을 선택하세요.");
+                return;
+            }
+
+            // 삭제시작!
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Helpers.Common.CONNSTRING))
+                {
+                    conn.Open();
+
+                    var delRes = 0;
+
+                    foreach (Favorite item in GrdResult.SelectedItems)
+                    {
+                        SqlCommand cmd = new SqlCommand(Models.Favorite.DELETE_QUERY, conn);
+                        cmd.Parameters.AddWithValue("@Id", item.Id);
+
+                        delRes += cmd.ExecuteNonQuery();
+                    }
+
+                    if (delRes == GrdResult.SelectedItems.Count)
+                    {
+                        await this.ShowMessageAsync("삭제", $"즐겨찾기 {delRes}건 삭제");
+                    }
+                    else
+                    {
+                        await this.ShowMessageAsync("삭제", $"즐겨찾기 {GrdResult.SelectedItems.Count}건중 {delRes} 건 삭제");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await this.ShowMessageAsync("오류", $"즐겨찾기 삭제 오류 {ex.Message}");
+            }
+
+            BtnViewFavorite_Click(sender, e); // 즐겨찾기 보기 재실행!
         }
     }
 }
---
+++ day08/wpf08_project/Daegu_Restaurant/Models/Favorite.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daegu_Restaurant.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; } // 추가생성, DB에 넣을때 사용할 값
+        public int Cnt { get; set; } // 순번
+        public string Opendata_id { get; set; } // 고유 번호
+        public string Gng_cs { get; set; } // 주소
+        public string Fd_cs { get; set; } // 음식카테고리
+        public string Bz_nm { get; set; } // 음식점명
+        public string Tlno { get; set; } // 연락처
+        public string Mbz_hr { get; set; } // 영업시간
+        public string Seat_cnt { get; set; } // 좌석수
+        public string Pkpl { get; set; } // 주차장
+        public string Hp { get; set; } // 웹사이트
+        public string Psb_frn { get; set; } // 가능언어
+        public string Bkn_yn { get; set; } // 예약가능여부
+        public string Infn_fcl { get; set; } // 놀이시설여부
+        public string Brft_yn { get; set; } // 조식여부
+        public string Dssrt_yn { get; set; } // 후식여부
+        public string Mnu { get; set; } // 메뉴
+        public string Smpl_desc { get; set; } // 설명
+        public string Sbw { get; set; } // 지하철 오시는길
+        public string Bus { get; set; } // 버스 오시는길
+
+        public static readonly string CHECK_QUERY = @"SELECT COUNT(*)
+                                                        FROM Favorite
+                                                       WHERE Id = @Id";
+
+        public static readonly string SELECT_QUERY = @"SELECT [Id]
+                                                            ,[Cnt]
+                                                            ,[Opendata_id]
+                                                            ,[Gng_cs]
+                                                            ,[Fd_cs]
+                                                            ,[Bz_nm]
+                                                            ,[Tlno]
+                                                            ,[Mbz_hr]
+                                                            ,[Seat_cnt]
+                                                            ,[Pkpl]
+                                                            ,[Hp]
+                                                            ,[Psb_frn]
+                                                            ,[Bkn_yn]
+                                                            ,[Infn_fcl]
+                                                            ,[Brft_yn]
+                                                            ,[Dssrt_yn]
+                                                            ,[Mnu]
+                                                            ,[Smpl_desc]
+                                                            ,[Sbw]
+                                                            ,[Bus]
+                                                        FROM [dbo].[Favorite]";
+
+        public static readonly string INSERT_QUERY = @"INSERT INTO [dbo].[Favorite]
+                                                                        ([Cnt]
+                                                                        ,[Opendata_id]
+                                                                        ,[Gng_cs]
+                                                                        ,[Fd_cs]
+                                                                        ,[Bz_nm]
+                                                                        ,[Tlno]
+                                                                        ,[Mbz_hr]
+                                                                        ,[Seat_cnt]
+                                                                        ,[Pkpl]
+                                                                        ,[Hp]
+                                                                        ,[Psb_frn]
+                                                                        ,[Bkn_yn]
+                                                                        ,[Infn_fcl]
+                                                                        ,[Brft_yn]
+                                                                        ,[Dssrt_yn]
+                                                                        ,[Mnu]
+                                                                        ,[Smpl_desc]
+                                                                        ,[Sbw]
+                                                                        ,[Bus])
+                                                                    VALUES
+                                                                        (@Cnt
+                                                                        ,@Opendata_id
+                                                                        ,@Gng_cs
+                                                                        ,@Fd_cs
+                                                                        ,@Bz_nm
+                                                                        ,@Tlno
+                                                                        ,@Mbz_hr
+                                                                        ,@Seat_cnt
+                                                                        ,@Pkpl
+                                                                        ,@Hp
+                                                                        ,@Psb_frn
+                                                                        ,@Bkn_yn
+                                                                        ,@Infn_fcl
+                                                                        ,@Brft_yn
+                                                                        ,@Dssrt_yn
+                                                                        ,@Mnu
+                                                                        ,@Smpl_desc
+                                                                        ,@Sbw
+                                                                        ,@Bus)";
+
+        public static readonly string DELETE_QUERY = @"DELETE FROM [dbo].[Favorite] WHERE Id = @id";
+    }
+}
